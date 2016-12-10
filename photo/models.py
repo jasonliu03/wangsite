fbb66bff9afd12e92026ed4ac5c53e3d496e8e9a
root <ziqidonglai03@163.com>
2016-12-10 18:19:48 +0800

    add .gitignore ...; add photo models;

--- photo/models.py
+++ photo/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class photo(models.Model):
+	name = models.CharField(max_length=30)
+	description = models.CharField(max_length=200)
+
+	
+
